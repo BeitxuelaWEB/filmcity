0adacf4bfc348a4d2f292a74c7a9d824778c62c3
Iris <iris.vilaseca1998@gmail.com>
2022-03-22 10:49:45 +0100

    prueba

--- src/main/java/org/factoriaf5/filmcity/controllers/MoviesController.java
+++ src/main/java/org/factoriaf5/filmcity/controllers/MoviesController.java
@@ -4,6 +4,8 @@ import org.factoriaf5.filmcity.domain.Movie;
 import org.factoriaf5.filmcity.repositories.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,5 +23,10 @@ public class MoviesController {
     @GetMapping("/movies")
     public List<Movie> allMovies() {
         return movieRepository.findAll();
+    }
+
+    @PostMapping("/movies")
+    public Movie addMovie(@RequestBody Movie movie) {
+        return movieRepository.save(movie);
     }
 }
